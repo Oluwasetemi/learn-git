b70bc1acbe46f1a7999064161cd70f2d77c1b04b
Oluwasetemi <osojo@student.lautech.edu.ng>
2018-08-25 14:13:05 +0100

    looped welcome to git

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-console.log('Welcome to git')
-console.log('Welcome to git')
+/* console.log('Welcome to git')
+console.log('Welcome to git') */
+
+for(let i = 0; i < 7; i++) {
+  console.log('Welcome to git')
+}
